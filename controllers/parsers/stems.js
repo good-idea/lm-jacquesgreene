b6a23f5087a8070d9bae4c1ccdf22cfb894bed13
Joseph Thomas <info@standard-quality.biz>
2017-04-15 00:01:01 -0700

    Stems full build

--- controllers/parsers/stems.js
+++ controllers/parsers/stems.js
@@ -3,13 +3,13 @@ const path = require('path');
 const stems = function stems(input) {
 	const content = Object.assign({}, input);
 
-	const rootDir = content.stems.rootDir;
+	const rootDir = content.stems.rootDir.replace(/https?:\/\//, '');
 	content.stems.groups.map((group) => {
 		const groupDir = group.dir;
 		group.links.map((link) => {
 			const filename = link.stemFileName;
 			link.display = link.stemTitle || link.stemFileName;
-			link.href = path.join(rootDir, groupDir, filename);
+			link.href = `//${path.join(rootDir, groupDir, filename)}`;
 		});
 	});
 
